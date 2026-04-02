abbb0c5946f608f077fd761c4784aecd71b543c1
Artur Brenner Schmitt <arturbrenner@gmail.com>
2026-04-02 13:41:12 -0300

    Subindo arquivos novos

---
+++ src/api/a-feira/controllers/a-feira.ts
@@ -0,0 +1,7 @@
+/**
+ * a-feira controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::a-feira.a-feira');
---
+++ src/api/a-feira/routes/a-feira.ts
@@ -0,0 +1,7 @@
+/**
+ * a-feira router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::a-feira.a-feira');
